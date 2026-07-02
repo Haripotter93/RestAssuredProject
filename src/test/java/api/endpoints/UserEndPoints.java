31de7620050930a72054897c76184488b8766595
Haripotter93 <hariharan.a@inapp.com>
2026-07-02 12:04:19 +0530

    Commit for adding yml file for GitHub Actions

--- src/test/java/api/endpoints/UserEndPoints.java
+++ src/test/java/api/endpoints/UserEndPoints.java
@@ -9,39 +9,41 @@ import io.restassured.response.Response;
 import static io.restassured.RestAssured.given;
 
 public class UserEndPoints {
-	
+
 	public static Response createUser(User userPayload) {
-		Response response=given()
-		.contentType (ContentType.JSON)
-		.accept(ContentType.JSON) .body(userPayload)
-		.when()
-		.post(Routes.post_url);
-		return response;
-	}
-	
-	public static Response readUser(String userName) {
-		Response response=given()
-		.pathParam("username", userName)
-		.when()
-		.get(Routes.get_url);
+		Response response = given()
+								.contentType(ContentType.JSON)
+								.accept(ContentType.JSON)
+								.body(userPayload)
+							.when()
+								.post(Routes.post_url);
 		return response;
 	}
 
+	public static Response readUser(String userName) {
+		Response response = given()
+								.pathParam("username", userName)
+							.when()
+								.get(Routes.get_url);
+		return response;
+	}
 
 	public static Response updateUser(String userName, User payload) {
-		Response response=given()
-		.contentType (ContentType.JSON) .accept(ContentType.JSON)
-		.pathParam("username", userName) .body (payload)
-		.when()
-		.put(Routes.update_url);
+		Response response = given()
+								.contentType(ContentType.JSON)
+								.accept(ContentType.JSON)
+								.pathParam("username", userName)
+								.body(payload)
+							.when()
+								.put(Routes.update_url);
 		return response;
 	}
-	
+
 	public static Response deleteUser(String userName) {
-		Response response=given()
-		.when()
-		.pathParam("username", userName)
-		.delete(Routes.delete_url);
+		Response response = given()
+							.when()
+								.pathParam("username", userName)
+								.delete(Routes.delete_url);
 		return response;
 	}
 
